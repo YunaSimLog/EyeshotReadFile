f454e9609c5fe32f69760afc5eba0f0c1a51f806
yunasim <ynsim@solidit.co.kr>
2024-04-16 22:44:35 +0900

    #😎update: STEP 파일 불러오기 구현

--- Form1.cs
+++ Form1.cs
@@ -24,14 +24,11 @@ namespace ReadFileSample
 
         protected override void OnLoad(EventArgs e)
         {
-            //devDept.Eyeshot.Translators.ReadSTL readSTL = new devDept.Eyeshot.Translators.ReadSTL(@"../../SampleFile/파트1.STL");
-            devDept.Eyeshot.Translators.ReadSTL readSTL = new devDept.Eyeshot.Translators.ReadSTL(@"../../SampleFile/멀티 바디.STL");
-            readSTL.DoWork();
+            //LoadSTL();
 
-            _entity = readSTL.Entities[0];
-            //_entity.Selected = true;
+            LoadSTEP();
 
-            design1.Entities.Add(_entity, Color.Bisque);
+            
             design1.ZoomFit();
             design1.SetView(devDept.Eyeshot.viewType.Isometric);
 
@@ -41,8 +38,59 @@ namespace ReadFileSample
             string axesLayer = "Axes";
             design1.Layers.Add(new Layer(axesLayer, Color.DeepPink, lineTypeName, 1, true));
 
-            design1.Selection.Color = Color.DarkGreen;
+            design1.SelectionFilterMode = selectionFilterType.Vertex
+                              | selectionFilterType.Edge
+                              | selectionFilterType.Face;
+
+            //design1.Selection.Color = Color.DarkGreen;
             base.OnLoad(e);
+        }
+
+        private void LoadSTEP()
+        {
+            devDept.Eyeshot.Translators.ReadSTEP readSTEP = new devDept.Eyeshot.Translators.ReadSTEP(@"../../SampleFile/Part1.STEP");
+            readSTEP.DoWork();
+
+            var entity = readSTEP.Entities[0];
+            Brep brep = entity as Brep;
+
+            brep.Selected = true;
+            brep.ClearFacesSelection();
+            brep.SelectionMode = selectionFilterType.Face;
+
+            design1.Entities.Add(brep, Color.Aqua);
+
+            Console.WriteLine($"모델 면 수 {brep.Faces.Length}");
+
+            for (int i = 0; i < brep.Faces.Length; i++)
+            {
+                if (i % 2 == 0)
+                    continue;
+
+                // 홀수 인덱스 면만 선택
+                brep.SetFaceSelection(i, true);
+            }
+
+            design1.Invalidate();
+        }
+
+        private void LoadSTL()
+        {
+            //devDept.Eyeshot.Translators.ReadSTL readSTL = new devDept.Eyeshot.Translators.ReadSTL(@"../../SampleFile/파트1.STL");
+            devDept.Eyeshot.Translators.ReadSTL readSTL = new devDept.Eyeshot.Translators.ReadSTL(@"../../SampleFile/멀티 바디.STL");
+            readSTL.DoWork();
+
+            _entity = readSTL.Entities[0];
+            var mesh = (Mesh)_entity;
+
+            //_entity.Selected = true;
+
+            //mesh.SetFaceSelection(32, 2, true);
+            var meshfaces = mesh.Faces;
+
+            //mesh.Selected = true;
+
+            design1.Entities.Add(_entity, Color.Bisque);
         }
     }
 }
